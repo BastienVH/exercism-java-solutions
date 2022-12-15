9da1178bb972288e92586ec9b5d8e86e61c87cb9
BastienVH <bastienvanhoudt@gmail.com>
2022-12-15 16:01:24 +0100

    added solution for need-for-speed

---
+++ need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,45 @@
+class NeedForSpeed {
+    private int speed;
+    private int batteryDrain;
+    private int distance;
+    private int batteryCapacity = 100;
+
+    public NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+    }
+
+    public boolean batteryDrained() {
+        return (batteryCapacity == 0);
+    }
+
+    public int distanceDriven() {
+        return distance;
+    }
+
+    public void drive() {
+        if (batteryDrained()) return;
+        distance += speed;
+        batteryCapacity -= batteryDrain;
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+
+    public int getMaxDistance() {
+        return speed * batteryCapacity / batteryDrain;
+    }
+}
+
+class RaceTrack {
+    private int distance;
+
+    public RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean tryFinishTrack(NeedForSpeed car) {
+        return (car.getMaxDistance() >= distance);
+    }
+}
