1b30753c19e8cf70a1d57f143a383fcdb034dacb
BastienVH <bastienvanhoudt@gmail.com>
2022-12-15 15:23:42 +0100

    solved remote-comtrol-competition

--- remote-control-competition/src/main/java/ProductionRemoteControlCar.java
+++ remote-control-competition/src/main/java/ProductionRemoteControlCar.java
@@ -1,18 +1,29 @@
-class ProductionRemoteControlCar {
+import java.io.Console;
+
+class ProductionRemoteControlCar implements RemoteControlCar, Comparable<ProductionRemoteControlCar> {
+    private int distanceTravelled;
+
+    private int numberofVictories;
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the ProductionRemoteControlCar.drive() method");
+        distanceTravelled += 10;
     }
 
     public int getDistanceTravelled() {
-        throw new UnsupportedOperationException("Please implement the ProductionRemoteControlCar.getDistanceTravelled() method");
+        return distanceTravelled;
     }
 
     public int getNumberOfVictories() {
-        throw new UnsupportedOperationException("Please implement the ProductionRemoteControlCar.getNumberOfVictories() method");
+        return numberofVictories;
     }
 
     public void setNumberOfVictories(int numberofVictories) {
-        throw new UnsupportedOperationException("Please implement the ProductionRemoteControlCar.setNumberOfVictories() method");
+        this.numberofVictories = numberofVictories;
+    }
+
+    @Override
+    public int compareTo(ProductionRemoteControlCar car) {
+        if (this.getNumberOfVictories() > car.getNumberOfVictories()) return 1;
+        return -1;
     }
 }
